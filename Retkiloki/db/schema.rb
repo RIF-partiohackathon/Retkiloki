59959dbd8419d1e4d5840834f8310b9daa49c0bf
Fleuri <laur1.suomalainen@gmail.com>
2014-10-25 10:09:13 +0300

    Push little, push often. Lisätty nimi eventtiin ja muokattu event ja type vieweja

--- Retkiloki/db/schema.rb
+++ Retkiloki/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024235445) do
+ActiveRecord::Schema.define(version: 20141025065446) do
 
   create_table "age_groups", force: true do |t|
     t.string   "group"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141024235445) do
     t.integer  "attendees"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "types", force: true do |t|
