63a2142243c337defddced02d6677efbc6f8c76c
ROTanner <roopetanner@hotmail.com>
2014-10-25 21:05:42 +0300

    Enabled Age Groups in User Edit

--- Retkiloki/db/seeds.rb
+++ Retkiloki/db/seeds.rb
@@ -6,17 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create([{ name: 'Ilari', phone: 'Eioo', email: 'mauri@hakkeri.net'}])
-User.create([{ name: 'Roope', phone: '123456789', email: 'fiat@hakkeri.net'}])
-User.create([{ name: 'Leo', phone: '020202', email: 'mopo@hakkeri.net'}])
-User.create([{ name: 'Fleuri', phone: '0700123123', email: 'fleuri@hakkeri.net'}])
-
 AgeGroup.create([{ group: 'Sudenpennut'}])
 AgeGroup.create([{ group: 'Seikkailijat'}])
 AgeGroup.create([{ group: 'Tarpojat'}])
 AgeGroup.create([{ group: 'Samoajat'}])
 AgeGroup.create([{ group: 'Vaeltajat'}])
 AgeGroup.create([{ group: 'Aikuiset'}])
+
+User.create([{ name: 'Ilari', phone: 'Eioo', email: 'mauri@hakkeri.net', :age_group => ( AgeGroup.find_by group: 'Aikuiset')}])
+User.create([{ name: 'Roope', phone: '123456789', email: 'fiat@hakkeri.net', :age_group => ( AgeGroup.find_by group: 'Sudenpennut')}])
+User.create([{ name: 'Leo', phone: '020202', email: 'mopo@hakkeri.net', :age_group => ( AgeGroup.find_by group: 'Tarpojat')}])
+User.create([{ name: 'Fleuri', phone: '0700123123', email: 'fleuri@hakkeri.net', :age_group => ( AgeGroup.find_by group: 'Vaeltajat')}])
 
 Event.create([{ name: 'Päiväretki', startdate: '1.10.2014', enddate: '1.10.2014'}])
 Event.create([{ name: '2 yön retki', startdate: '1.10.2014', enddate: '3.10.2014'}])
