a883999c7d1eabe0e36a939619edb99720aebd30
Fleuri <laur1.suomalainen@gmail.com>
2014-10-25 20:00:00 +0300

    Attendanceja ja committeja

--- Retkiloki/app/controllers/attendances_controller.rb
+++ Retkiloki/app/controllers/attendances_controller.rb
@@ -69,6 +69,6 @@ class AttendancesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def attendance_params
-      params.require(:attendance).permit(:startdate, :enddate)
+      params.require(:attendance).permit(:startdate, :enddate, :user_id, :event_id)
     end
 end
