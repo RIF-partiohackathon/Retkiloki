3249e49fb1b4c2a0b442c8a0d639a4c9c49bf147
Leo Lahti <leo.lahti1@gmail.com>
2014-10-25 13:59:56 +0000

    Added advanced daycalculator

--- Retkiloki/app/helpers/events_helper.rb
+++ Retkiloki/app/helpers/events_helper.rb
@@ -1,15 +1,24 @@
 module EventsHelper
 	def countdays() #counts total days
+		attendees = Attendance.where(event_id = @event_id)
+		fulltimeattendances = 0
+		parttimeattendances = 0
+		parttimedays = 0
+		for i in attendees
+			if (i.enddate = @event.enddate && i.startdate == @event.startdate)
+				fulltimeattendances = fulltimeattendances +1
+			else
+				parttimeattendances = parttimeattendances + 1
+				parttimedays = parttimedays +( i.enddate - i.startdate)
+			end
+		end
 		if((@event.enddate - @event.startdate) < 0 || @event.attendees <= 0)
 			return "You gave negative duration and/or zero attendees"
 		elsif((@event.enddate - @event.startdate) == 1)
 			return 	@event.attendees
 		elsif((@event.enddate - @event.startdate) > 1)
-			return @event.attendees * (@event.enddate - @event.startdate).to_int
-		
-		else
-			return @event.attendees.to_int
+			return attendees * (@event.enddate - @event.startdate)+parttimedays.to_int
+
 		end
 	end
-
 end
