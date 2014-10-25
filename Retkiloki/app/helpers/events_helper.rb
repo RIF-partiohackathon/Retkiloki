d1f6690220fee618f253cae20de03ce55c6403a8
Leo Lahti <leo.lahti1@gmail.com>
2014-10-25 18:36:23 +0000

    fhabl

--- Retkiloki/app/helpers/events_helper.rb
+++ Retkiloki/app/helpers/events_helper.rb
@@ -1,15 +1,15 @@
-odule EventsHelper
+module EventsHelper
 	def countdays() #counts total days
 		attendees = Attendance.where(event_id = @event_id)
 		fullTimeAttendances = 0
-		PartTimeAttendances = 0
-		PartTimedays = 0
+		partTimeAttendances = 0
+		partTimedays = 0
 		for i in attendees
 			if (i.enddate = @event.enddate && i.startdate == @event.startdate)
 				fullTimeAttendances = fulltimeAttendances + 1
 			else
-				PartTimeAttendances = PartTimeattendances + 1
-				PartTimedays = PartTimedays + ( i.enddate - i.startdate)
+				partTimeAttendances =partTimeattendances + 1
+				partTimedays = partTimedays + ( i.enddate - i.startdate)
 			end
 		end
 		if((@event.enddate - @event.startdate) < 0 || @event.attendees == nil || @event.attendees < 1)
@@ -17,7 +17,7 @@ odule EventsHelper
 		elsif((@event.enddate - @event.startdate) == 1)
 			return 	@event.attendees
 		elsif((@event.enddate - @event.startdate) > 1)
-			return attendees * ((@event.enddate - @event.startdate)+PartTimedays).to_int
+			return @event.attendees * ((@event.enddate - @event.startdate)-partTimedays).to_int
 
 		end
 	end
