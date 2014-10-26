1047efa5b9d72afada00430d84f5076578256e14
Leo Lahti <leo.lahti1@gmail.com>
2014-10-26 08:31:14 +0000

    flh<s

--- Retkiloki/app/helpers/users_helper.rb
+++ Retkiloki/app/helpers/users_helper.rb
@@ -19,13 +19,13 @@ module UsersHelper
 		leirit = 0
 		pitka_vaellus = 0
  
-		@user.attendances.each do |atte|
+		for atte in @user.attendances
 			metsayoRetket += 1 if (atte.enddate - atte.startdate) > 0
-			pt_kisat_sm += 1 if atte.attendances.event.type == pt_kisat_sm
-			pt_kisat_pm += 1 if atte.attendances.event.type == pt_kisat_pm
-			pt_kisat_muu += 1 if atte.attendances.event.type == pt_kisat_muu
-			leirit += 1 if atte.attendances.event.type == leiri
-			pitka_vaellus += 1 if atte.attendances.type == pitka_vaellus
+			pt_kisat_sm += 1 if atte.event.type == pt_kisat_sm
+			pt_kisat_pm += 1 if atte.event.type == pt_kisat_pm
+			pt_kisat_muu += 1 if atte.event.type == pt_kisat_muu
+			leirit += 1 if atte.event.type == leiri
+			pitka_vaellus += 1 if atte.event.type == pitka_vaellus
 		end
  
 		if pitka_vaellus && metsayoRetket > 24 && leirit > 9 && (pt_kisat_sm+pt_kisat_sm+pt_kisat_muu) > 9 && pt_kisat_sm
