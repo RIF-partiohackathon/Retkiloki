08da105d68a9a4f84c825449f6e04df0e1573756
Leo Lahti <leo.lahti1@gmail.com>
2014-10-26 07:56:47 +0000

    Fixed event_helper.rb. Again

--- Retkiloki/app/helpers/events_helper.rb
+++ Retkiloki/app/helpers/events_helper.rb
@@ -4,7 +4,9 @@ module EventsHelper
     #counts total days
     attendees = Attendance.where(event_id = @event.id)
     days = 0
-    if !attendees.nil?
+    tmpattendees = 0
+    if !@event.attendees.nil?
+    if !attendees.nil? 
       attendees.each do |i|
           tmpdays = (i.enddate - i.startdate)
             if tmpdays <= 1
@@ -13,17 +15,22 @@ module EventsHelper
               days += (tmpdays - 1)
             end
       end
-      tmpattendees = @event.attendees - attendees.size;
+      tmpattendees = @event.attendees - attendees.length;
     else
       tmpattendees = @event.attendees
     end
+    if tmpattendees > 0
+		
     tmpdays = tmpattendees * (@event.enddate - @event.startdate)
     if tmpdays <= 1
       days +=1
     else
       days += (tmpdays - 1)
     end
+    end
     return days
+    end
+    return "Ei osallistujamaaraa"
   end
 
 end
