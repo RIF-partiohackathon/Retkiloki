8717fe0bc600bc84aec980a0d468afec2e7e2018
Fleuri <laur1.suomalainen@gmail.com>
2014-10-26 16:38:48 +0200

    Generoitu testipohja userille

--- Retkiloki/app/helpers/application_helper.rb
+++ Retkiloki/app/helpers/application_helper.rb
@@ -2,9 +2,36 @@ module ApplicationHelper
 
   def countAllEventDays
     event = Event.all;
-    days
+    days = 0
     event.each do |f|
-      f.countdays
+     days = countdays2(f)
     end
+    return days;
   end
+
+  def countdays2(event)
+  #counts total days
+  attendees = Attendance.where(event_id = event.id)
+  days = 0
+  if !attendees.nil?
+    attendees.each do |i|
+      tmpdays = (i.enddate - i.startdate)
+      if tmpdays <= 1
+        days +=1
+      else
+        days += (tmpdays - 1)
+      end
+    end
+    tmpattendees = event.attendees - attendees.size
+  else
+    tmpattendees = event.attendees
+  end
+  tmpdays = tmpattendees * (event.enddate - event.startdate)
+  if tmpdays <= 1
+    days +=1
+  else
+    days += (tmpdays - 1)
+  end
+  return days
+end
 end
